6b06ee956f517f32d9e86ecf6f049e3b93b1647f
MinhTri <dhmt721305@gmail.com>
2025-06-03 13:21:49 +0700

    Get user by email

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,35 @@
+import User from "@/database/user.model";
+import { handleError } from "@/lib/handler/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  //   dbConnect();
+  const { email } = await request.json();
+
+  try {
+    const validatedData = UserSchema.partial().safeParse({ email });
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const user = await User.findOne({ email });
+    console.log("email", user);
+
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: user,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
